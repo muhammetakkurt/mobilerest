28cf4e3566ca88164175543773e2577530b9ddda
Muhammet Akkurt <muhammetakkurt@Muhammets-MacBook-Pro.local>
2016-06-13 00:46:05 +0300

    test

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 
 var krms_config ={	
-	'ApiUrl' : "http://acliginigider.com/mobileapp/api",
+	'ApiUrl' : "http://acliginigider.com/merchantapp/api",
 	'DialogDefaultTitle' : "Açlığını Gider",
 	'pushNotificationSenderid' : "729826487233",
 	'APIHasKey' : "91a82634be7e9a661503f1da5fb159d0"
