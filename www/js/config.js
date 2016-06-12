7e2e4e47265f1a857f6bbe76062a53bf5f015ac1
Muhammet Akkurt <muhammetakkurt@Muhammets-MacBook-Pro.local>
2016-06-12 18:58:58 +0300

    mesaj

--- www/js/config.js
+++ www/js/config.js
@@ -2,6 +2,6 @@
 var krms_config ={	
 	'ApiUrl' : "http://acliginigider.com/mobileapp/api",
 	'DialogDefaultTitle' : "Açlığını Gider",
-	'pushNotificationSenderid' : "YOUR_ANDROID_PUSH_PROJECT_ID",
+	'pushNotificationSenderid' : "729826487233",
 	'APIHasKey' : "91a82634be7e9a661503f1da5fb159d0"
 };
